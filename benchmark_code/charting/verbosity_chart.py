e402e0601588c8cd6951d4a3a04d94a673dc8b81
shai <shai_rubin@yahoo.com>
2025-05-20 08:13:51 +0300

    need to replace 'message' with file summary

--- benchmark_code/charting/verbosity_chart.py
+++ benchmark_code/charting/verbosity_chart.py
@@ -1,7 +1,7 @@
 import os
 from collections import defaultdict
 from math import floor
-
+from dataclasses import dataclass
 from benchmark_code.llm_model import AVAILABLE_MODELS
 from benchmark_code.utils import get_db
 from chart_generator import ChartGenerator
@@ -9,35 +9,38 @@ from benchmark_code import project_name
 
 debug = False
 supported_models = [x.known_name for x in AVAILABLE_MODELS]
-def default_model_stats():
-    return {
-        "tokens_used": [],
-        "single_input_token_cost": None,
-        "single_output_token_cost": None,
-        "total_input_cost": 0.0,
-        "total_output_cost": 0.0,
-        "total_input_tokens": 0,
-        "total_output_tokens": 0,
-        "avg_file_cost": 0.0,
+@dataclass
+class SingleFileSummarizationData:
+    number_of_words: int = 0
         
-    }
+
+# def default_model_stats():
+#     return {
+#         "tokens_used": [],
+#         "single_input_token_cost": None,
+#         "single_output_token_cost": None,
+#         "total_input_cost": 0.0,
+#         "total_output_cost": 0.0,
+#         "total_input_tokens": 0,
+#         "total_output_tokens": 0,
+#         "avg_file_cost": 0.0,
+#         "verbosity_score": 0.0, # avarage words per file in output 
+        
+#     }
 
 def get_model_stats(model_data):
     
-    model_stats = defaultdict(default_model_stats)
-    for file_name, file_data in model_data.items():
-        for model_name, m_data in file_data.items():
+    model_stats = defaultdict(list)
+    for file_name, file_sumarization_data in model_data.items():
+        for model_name, model_sumarization_data in file_sumarization_data.items():
             if not model_name in supported_models:
                 raise ValueError(f"{model_name=}  is not supported")
-            model_stats[model_name]["tokens_used"].append(m_data["total_tokens"])
-            if model_stats[model_name]["single_input_token_cost"] == None:
-                model_stats[model_name]["single_input_token_cost"] = m_data['model']["price_per_1000_input_tokens"] / 1000
-            if model_stats[model_name]["single_output_token_cost"] == None:
-                model_stats[model_name]["single_output_token_cost"] = m_data['model']["price_per_1000_output_tokens"] / 1000
-            assert model_name == m_data['model']['known_name']
-    for model_name, stats in model_stats.items():
-        if stats["single_input_token_cost"] == None or stats["single_output_token_cost"] == None:
-            raise ValueError(f"{model_name=} has no valid cost data")
+            input_file_summary = model_sumarization_data.get("file_summary", None)
+            if input_file_summary is None:
+                raise ValueError(f"{file_name=} has no input file summary")
+            number_of_words = len(input_file_summary.split())
+            assert number_of_words > 0, f"{file_name=} has no words in input file summary"
+            model_stats[model_name].append(number_of_words)
     return model_stats
 
 
